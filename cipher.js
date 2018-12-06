2a2fc61ffec7e266a35c54d2b34f9612054ada78
ConiPinto <c.pinjazz@gmail.com>
2018-12-06 11:46:35 -0300

    proyecto con test listos

--- cipher.js
+++ cipher.js
@@ -1,8 +1,99 @@
+
+
+
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  
+  encode: (textoCifrar, desplazamiento) => {
+    let texto = "";
+    for (let i = 0; i< textoCifrar.length; i++){
+    
+    let textoSegundo = textoCifrar.charCodeAt(i);
+    if(textoSegundo >=65 && textoSegundo<=90){
+      let code = (textoSegundo-65+desplazamiento)%26+65;
+      texto += String.fromCharCode(code);
+
+
+    }
+    }
+    return texto;
+
   },
-  decode: () => {
-    /* Acá va tu código */
+
+
+  
+  decode: (textoCifrar, desplazamiento) => {
+    let texto = "";
+    for (let i = 0; i< textoCifrar.length; i++){
+
+    let textoSegundo = textoCifrar.charCodeAt(i);
+    
+    if(textoSegundo >=65 && textoSegundo<=90){
+      let code = (textoSegundo-65-desplazamiento)%26+65;
+
+      if (code<65){
+        code+=26;
+      }
+      texto += String.fromCharCode(code);
+    }
   }
+  return texto;
+}
 };
+
+
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*window.cipher = { 
+  encode: (textoCifrar, desplazamiento) => {
+    let textToEncrypt="";
+    for (let i=0; i< textoCifrar.length;i++){
+      let textoDos=textoCifrar.charCodeAt(i);
+        if (textoDos >=65 && textoDos<=90){
+        let newCode=(textoDos-65+desplazamiento)%26+65;
+        textToEncrypt+=String.fromCharCode(newCode);
+      }
+          }
+    return textToEncrypt;
+ 
+ 
+  },
+  decode: (textoCifrar, desplazamiento) => {
+    let textToEncrypt="";
+    for (let i=0; i< textoCifrar.length;i++){
+      let textoDos=textoCifrar.charCodeAt(i);
+        if (textoDos >=65 && textoDos<=90){
+        let newCode=(textoDos-65-desplazamiento)%26+65;
+        if (newCode<65){
+          newCode+=26;
+        }
+        textToEncrypt+=String.fromCharCode(newCode);
+      }
+          }
+    return textToEncrypt;
+  }
+ 
+ };*/
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-/* Acá va tu código */
+
+window.onload = () => {
+  
+  document.getElementById("botoncodificar").addEventListener("click", () => {
+      let encryptText = document.getElementById("textoCifrar").value;
+      let offset = document.getElementById("desplazamiento").value;
+      offset=parseInt(offset);
+
+      document.getElementById("resultado").innerHTML=(cipher.encode(encryptText, offset));
+    
+    }),
+
+
+    
+    document.getElementById("botondescifrar").addEventListener("click", () => {
+        let encryptText = document.getElementById("textoCifrar").value;
+        let offset = document.getElementById("desplazamiento").value;
+        offset=parseInt(offset);
+        
+        document.getElementById("resultado").innerHTML=cipher.decode(encryptText, offset);
+    
+    })}
